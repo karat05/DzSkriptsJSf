a221845d2393791ea9627b90d7c4aa1ce5885b3b
Рыжов Ярослав Романович <29455618@s.mfui.ru>
2026-02-03 12:02:16 +0300

    frjkr

---
+++ class.js
@@ -0,0 +1,15 @@
+let a = prompt();
+
+switch (a) {
+    case 3:
+    case 5:
+        alert("Рядом");
+        break;
+    case 4:
+        alert("В точку");
+        break;
+    
+    default:
+            alert("МММ");
+            break;
+}
