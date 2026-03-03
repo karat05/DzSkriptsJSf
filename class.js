21d495a03a098fccf546af15ddd9158d38398266
karat05 <yaric23x@gmail.com>
2026-03-03 16:12:06 +0300

    321

--- class.js
+++ class.js
@@ -1,12 +1,72 @@
 
-// filter(element, index) возращает новый массив, удовлетворяет условию( все четные числа)
-//функция филтр дробит каждый элемент на свое значение
-//previous.concat(next) слияние массивов
-//array.slice //срез массив
-const arrayFirst = [-3, -2, -1]
-const arraySecond = [0, 1, 2]
-const arrayThird = [10, 20, 30]
-const newArray = arrayFirst.concat(arraySecond).concat(arrayThird)
+// сделать фон красным
+//1) document.body.style.vackground = 'red'
+//2) document.getElementById( "id элемента") - поиск элемента по id
+//3) document.querySelector("body") - поиск gthdjuj ищвн dyenhb html
+//4) document.querySelector("#body") - поиск первого body по id внутри html
+//5) document.querySelector (".body") - поиск первого body по class внутри html 
+//innerHTML // возращает контент    //"<hi>Hello, world!</hi>"
+//body.innerHTML = "<hi>Hello, world!</hi>"    //небезопасно (xxs - уязвимость)
+//textCOntent = "" // возращает текстовое содержимое
+//addEventListener("событие",  "функция") // 
+//setAttribute ("Тип атрибута название")
+//removeAttribute (тип атрибута) //удаление атрибута
 
-console.log(newArray)
-console.log(newArray.slice(1))
+//ПРИМЕР ПО СОЗДАНИЮ ОБРАБОЧИКА СОБЫТИЙ click
+//const clicker = () => {
+  //  const div = document.querySelector("div")
+  //  div.textContent = "Hello,world"
+//}
+ 
+//document.querySelector("button").addEventListener("click", clicker)
+//clicker
+
+
+//document.getElementById("body").style.background = "red"
+//body.style.background = "red"
+//body.style.color = "white"
+
+
+//const clicker = () => {
+//const div = document.querySelector("div")
+//div.textContent = "Hello, world"  //безопасно
+//}
+//clicker
+//document.querySelector("button").addEventListener("click", clicker)
+
+//наход дочерного дива
+//const firstDiv = document.querySelector("div:nth-child(2)")
+//firstDiv.textContent = "hello"
+
+
+
+// работа с атрибутами class элементов
+// элемент.classlist.add/remove/toggle("название класса")
+//togle ("название") - добавляет класс,енсли отсуствует у элемента
+// или удаляет класс , если он уже имеется 
+
+//const clicker = () => {
+//const firstDiv = document.querySelector("div")
+//firstDiv.classList.toggle("black")
+//}
+
+
+
+//document.createElement
+
+
+
+//const firstDiv = document.querySelector("div")
+
+//const p = document.createElement("p")
+
+
+
+
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+        cell.classList.toggle("cell-active");
+    });
+});
